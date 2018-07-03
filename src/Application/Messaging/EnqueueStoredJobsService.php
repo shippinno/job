18bb852ab061cf5a0eace4affce0af506e70b351
Hirofumi Tanigami <tanigami@gmail.com>
2018-07-03 17:28:15 +0900

    i

--- src/Application/Messaging/EnqueueStoredJobsService.php
+++ src/Application/Messaging/EnqueueStoredJobsService.php
@@ -74,7 +74,11 @@ class EnqueueStoredJobsService
                     $message->setMessageDeduplicationId(uniqid());
                 }
                 if (method_exists($message, 'setMessageGroupId')) {
-                    $message->setMessageGroupId(is_null($storedJob->fifoGroupId()) ? uniqid() : $storedJob->fifoGroupId());
+                    $message->setMessageGroupId(
+                        is_null($storedJob->fifoGroupId())
+                            ? uniqid()
+                            : $storedJob->fifoGroupId()
+                    );
                 }
                 $producer->send($topic, $message);
                 $enqueuedMessagesCount = $enqueuedMessagesCount + 1;
